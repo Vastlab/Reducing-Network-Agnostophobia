2443d71d40188153c9f5b6ad0d969e850f39e343
akshay <adhamija@uccs.edu>
2018-04-04 00:05:07 -0600

    WIP

--- ImageNet/InceptionV3.py
+++ ImageNet/InceptionV3.py
@@ -3,36 +3,48 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--test_network", dest="test_network", action="store_true")
-parser.add_argument("--use_bg_cls", dest="use_bg_cls", action="store_true")
+parser.add_argument("--use_bg_cls", dest="use_bg_cls", action="store_true",default=False)
 parser.add_argument("--dont_include_known_unknowns", dest="include_known_unknowns", action="store_false", default=True)
 parser.add_argument("--split_no", action="store", dest="split_no", type=int, default = 0)
 parser.add_argument("--model", action="store", dest="use_model", default = None)
 parser.add_argument('--gpus', nargs='+', help='GPU No or Numbers for Multiple GPUs', required=True, type=int)
+parser.add_argument("--use_ring_loss", dest="use_ring_loss", action="store_true", default=False)
 args = parser.parse_args()
 
 print "split_no",args.split_no,args.use_bg_cls,"args.gpus",args.gpus,len(args.gpus)
 
 
 parallel=False
+Batch_Size = 128
+
+if len(args.gpus)>1:
+    Batch_Size=Batch_Size*len(args.gpus)
+    
+data_generator_params = dict(
+                                                            batch_size=Batch_Size,
+                                                            use_bg_cls=args.use_bg_cls,
+                                                            split_no=args.split_no
+                            )
+
+if args.use_ring_loss:
+    data_generator_params['unknownsMaximumMag'] = 5.
+    data_generator_params['knownsMinimumMag'] = 10.
 
 if parallel:
     from imagenet_data_prep import parallelized_imagenet_data_prep    
     training_generator = parallelized_imagenet_data_prep(
-                                            db_type='train',
-                                            batch_size=128,
-                                            include_known_unknowns = args.include_known_unknowns,
-                                            use_bg_cls=args.use_bg_cls,
-                                            split_no=args.split_no,
-                                            )
+                                                            db_type='train',
+                                                            include_known_unknowns = args.include_known_unknowns,
+                                                            shuffle=True,
+                                                            **data_generator_params
+                                                        )
 
     validation_generator = parallelized_imagenet_data_prep(
-                                            db_type='val',
-                                            batch_size=128,
-                                            training_data_obj=training_generator,
-                                            use_bg_cls=args.use_bg_cls,
-                                            split_no=args.split_no
-                                            )
+                                                            db_type='val',
+                                                            training_data_obj=training_generator,
+                                                            shuffle=False,
+                                                            **data_generator_params
+                                                        )
 
     fit_generator_params = dict(
                                 workers=1,
@@ -45,22 +57,20 @@ else:
     from imagenet_data_prep import imagenet_data_prep    
     training_generator = imagenet_data_prep(
                                             db_type='train',
-                                            batch_size=64,
                                             include_known_unknowns = args.include_known_unknowns,
-                                            use_bg_cls=args.use_bg_cls,
-                                            split_no=args.split_no
+                                            shuffle=True,
+                                            **data_generator_params
                                             )
 
     validation_generator = imagenet_data_prep(
-                                            db_type='val',
-                                            batch_size=64,
-                                            training_data_obj=training_generator,
-                                            use_bg_cls=args.use_bg_cls,
-                                            split_no=args.split_no
+                                                db_type='val',
+                                                training_data_obj=training_generator,
+                                                shuffle=False,
+                                                **data_generator_params
                                             )
     fit_generator_params = dict(
-                                workers=100,
-                                use_multiprocessing=True
+                                workers=1,
+                                use_multiprocessing=False
                                )
     
 
@@ -68,18 +78,23 @@ import keras
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Flatten
+from keras.layers import Dense, GlobalAveragePooling2D, Flatten, Activation, Input
 from keras import backend as K
 from keras.optimizers import Adam
-
+from keras.callbacks import ModelCheckpoint
 
 from keras.backend.tensorflow_backend import set_session
 from keras.utils import multi_gpu_model
 import tensorflow as tf
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-set_session(tf.Session(config=config))
+
+
+
+if len(args.gpus)==1:
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.visible_device_list = str(args.gpus[0])
+    set_session(tf.Session(config=config))
 
 
 if args.use_model is None:
@@ -95,67 +110,141 @@ if args.use_model is None:
         x = Dense(1024,name='fc')(x)
 
         if args.use_bg_cls:
-            pred = Dense(101, activation='softmax',name='pred')(x)
+            pred = Dense(101,name='pred')(x)
+#            pred = Dense(101, activation='softmax',name='pred')(x)
         else:
-            pred = Dense(100, activation='softmax',name='pred')(x)
+            pred = Dense(100,name='pred')(x)
+#            pred = Dense(100, activation='softmax',name='pred')(x)
 
-        model = Model(inputs=base_model.input, outputs=pred)
+        softmax_output = Activation('softmax',name='softmax')(pred)
+        model = Model(inputs=base_model.input, outputs=softmax_output)
+#        model = Model(inputs=base_model.input, outputs=pred)
 
 else:
     model = keras.models.load_model(args.use_model)
-    print ".....................DONE........................"
+    print "Loded Model",args.use_model
     if args.use_bg_cls and model.output_shape[1]!=101:
+        
+        # Adding additional node for the Background class to the fully connected layer
+        weights = model.get_layer('pred').get_weights()
+        # Adding the Bias Term
+        weights[1] = np.concatenate((weights[1],np.zeros(1)),axis=0)
+        # Adding the Weights and initializing them
+        weights[0] = np.concatenate((weights[0],-0.0001*np.random.random_sample((weights[0].shape[0],1)) + 0.0001),axis=1)
+        
         model.layers.pop()
         model.layers[-1].outbound_nodes = []
         model.outputs = [model.layers[-1].output]
         output = model.get_layer('fc').output
         #output = Flatten()(output)
-        #pred = Dense(101, activation='softmax',name='pred')(output)
-        pred = Dense(101, activation='softmax',name='pred')(output)
-        model = Model(inputs=model.input, outputs=pred)
+        pred = Dense(101,name='pred')(output)
+        softmax_output = Activation('softmax',name='softmax')(pred)
+        model = Model(inputs=base_model.input, outputs=softmax_output)
+#        model = Model(inputs=model.input, outputs=pred)
+        model.get_layer('pred').set_weights(weights)
         print ".....................DONE........................123"
     
-if len(args.gpus)>1:
-    parallel_model = multi_gpu_model(model, gpus=args.gpus)
-else:
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.gpu_options.visible_device_list = str(args.gpus[0])
-    set_session(tf.Session(config=config))
-    parallel_model = model
-
-
 for layer in model.layers[:249]:
     layer.trainable = False
 for layer in model.layers[249:]:
     layer.trainable = True
 
-#for layer in base_model.layers:
-#    layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
 
-adam = Adam(lr=0.001)
-parallel_model.compile(optimizer=adam, loss={'pred':'categorical_crossentropy'},metrics=['acc'])
+
+def ring_loss(y_true,y_pred):
+    pred=K.sqrt(K.sum(K.square(y_pred),axis=1))
+    error=K.mean(K.square(
+        # Loss for Knowns having magnitude greater than knownsMinimumMag
+        y_true[:,0]*(K.maximum(knownsMinimumMag-pred,0.))
+        # Add two losses
+        +
+        # Loss for unKnowns having magnitude greater than unknownsMaximumMag
+        y_true[:,1]*(K.maximum(pred-unknownsMaximumMag,0.))
+    ))
+    return error
 
 
+
+adam = Adam(lr=0.01)
+if args.use_ring_loss:
+    #base_model = InceptionV3(weights='imagenet', include_top=False)
+    #print "base_model.shape",base_model.input
+    #unknownsMaximumMag = Input(shape=(1,), dtype='float32', name='unknownsMaximumMag')
+    #knownsMinimumMag = Input(shape=(1,), dtype='float32', name='knownsMinimumMag')
+    print "model.input",model.input
+    #image = Input(shape=(299, 299, 3), dtype='float32', name='image')
+    #model.inputs=[model.input,unknownsMaximumMag,knownsMinimumMag]
+#    pred = model.get_layer('pred').output
+#    fc = model.get_layer('fc').output
+    """
+    model = Model(
+                    inputs=[
+                            model.input,
+#                            unknownsMaximumMag,
+#                            knownsMinimumMag
+                            ], 
+                    outputs=[
+                            pred, 
+#                            fc
+                            ]
+                 )
+    """
+    print "model.input",model.inputs
+    print data_generator_params
+    model.compile(
+                    optimizer=adam,
+                    loss={'pred': 'categorical_crossentropy'},#,'fc':ring_loss},
+#                    loss_weights={'pred': 1., 'fc': 0.00001},
+                    metrics=['accuracy']#,'accuracy']
+                )
+
+else:
+    model.compile(optimizer=adam, loss={'softmax':'categorical_crossentropy'},metrics=['acc'])
+
+
+"""
+if len(args.gpus)>1:
+    parallel_model = multi_gpu_model(model, gpus=args.gpus)
+else:
+    parallel_model = model
+"""
 # train the model on the new data for a few epochs
 #model.fit_generator(...)
-keras.backend.get_session().run(tf.global_variables_initializer())
-parallel_model.fit_generator(
+#keras.backend.get_session().run(tf.global_variables_initializer())
+
+model_file=''
+if not args.include_known_unknowns:
+    model_file='Pretrained_model/'
+elif args.use_bg_cls:
+    model_file='BG_Cls_Data/'
+elif args.use_ring_loss:
+    model_file='Ring_Loss_Data/'
+else:
+    model_file='Cross_Entr_Data/'
+
+
+"""
+checkpointer = ModelCheckpoint(
+                                filepath=model_file+'Model_Epoch_number_{}.hdf5', 
+                                verbose=1, 
+                                save_best_only=False , 
+                                period=1, 
+                                save_weights_only=False
+                                )
+"""
+print "Here"
+print "validation_generator",validation_generator#,next(validation_generator)
+print "training_generator",training_generator#,next(training_generator)
+model.fit_generator(
                             generator=training_generator,
                             validation_data=validation_generator,
                             max_queue_size=50,
-                            epochs=10,
+                            epochs=5,
+#                            callbacks=[checkpointer],
 #                            verbose=2,
                             **fit_generator_params
                             )
 
-
-if not args.include_known_unknowns:
-    model.save('Pretrained_model/Finetuned_Model_Split_'+str(args.split_no))
-else:
-    if args.use_bg_cls:
-        model.save('BG_Cls_Data/Finetuned_Model_Split_'+str(args.split_no))
-    else:
-        model.save('Cross_Entr_Data/Finetuned_Model_Split_'+str(args.split_no))
+#model.save(model_file)
