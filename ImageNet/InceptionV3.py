cf42bcc97cb82879497c68a8f55ac87e53da9a0c
akshay <adhamija@uccs.edu>
2018-04-15 23:21:51 -0600

    Supports multi GPU

--- ImageNet/InceptionV3.py
+++ ImageNet/InceptionV3.py
@@ -16,7 +16,7 @@ parser.add_argument("--snapshot_location", action="store", dest="snapshot_locati
 args = parser.parse_args()
 
 parallel=False
-Batch_Size = 32
+Batch_Size = 64
 
 if len(args.gpus)>1:
     Batch_Size=Batch_Size*len(args.gpus)
@@ -25,7 +25,7 @@ data_generator_params = dict(
                                 batch_size=Batch_Size,
                                 use_bg_cls=args.use_bg_cls,
                                 split_no=args.split_no,
-                                debug=True#False #True
+                                debug=False #True
                             )
 
 if args.use_ring_loss:
@@ -144,20 +144,18 @@ else:
         softmax_output = Activation('softmax',name='softmax')(pred)
         model = Model(inputs=model.input, outputs=softmax_output)
         model.get_layer('pred').set_weights(weights)
-    
+
+"""
 for layer in model.layers[:249]:
     layer.trainable = False
 for layer in model.layers[249:]:
     layer.trainable = True
-
-print "RUNNING"
-#for layer in model.layers[:1]:
-#    layer.trainable = False
 """
-for layer in model.layers:
-    print layer
+for layer in model.layers[:1]:
+    layer.trainable = False
+for layer in model.layers[1:]:
+#    print layer
     layer.trainable = True
-"""
 """
 for layer in model.layers[:100]:
     layer.trainable = False
@@ -166,11 +164,9 @@ for layer in model.layers[100:]:
 """
 # compile the model (should be done *after* setting layers to non-trainable)
 
-global_loss = []
 def ring_loss(y_true,y_pred):
     pred=K.sqrt(K.sum(K.square(y_pred),axis=1))
     error=K.mean(K.square(
-#    error=(K.square(
         # Loss for Knowns having magnitude greater than knownsMinimumMag
         y_true[:,0]*(K.maximum(knownsMinimumMag-pred,0.))
         # Add two losses
@@ -178,14 +174,7 @@ def ring_loss(y_true,y_pred):
         # Loss for unKnowns having magnitude greater than unknownsMaximumMag
         y_true[:,1]*(K.maximum(pred-unknownsMaximumMag,0.))
     ))
-#    global_loss.extend(error.tolist())
-#    print "error",error.shape
     return error
-
-
-def ard_mean_pred(y_true, y_pred):
-    print "y_pred",y_pred.shape
-    return y_pred
 
 
 adam = Adam(lr=0.01)
@@ -219,10 +208,8 @@ if args.use_ring_loss:
                             optimizer=adam,
                             loss={'softmax': 'categorical_crossentropy','fc':ring_loss},
                             loss_weights={'softmax': args.cross_entropy_loss_weight, 'fc': args.ring_loss_weight},
-#                            metrics=[ard_mean_pred,'acc']
                             metrics=['acc']
                         )
-    print "HERE"
 
 else:
     parallel_model.compile(optimizer=adam, loss={'softmax':'categorical_crossentropy'},metrics=['acc'])
@@ -237,66 +224,31 @@ if not args.include_known_unknowns:
 elif args.use_bg_cls:
     model_file='/home/adhamija/faceness/ImageNet/BG_Cls_Data/'+args.snapshot_location+common_name
 elif args.use_ring_loss:
-#    model_file='Ring_Loss_Data/Model_Epoch_number_'+str(args.cross_entropy_loss_weight)+'_'+str(args.ring_loss_weight)+'_{epoch:02d}.hdf5'
-#    model_file='Ring_Loss_Data/ALL_Layers_Model_Epoch_number_'+str(args.cross_entropy_loss_weight)+'_'+str(args.ring_loss_weight)+'_{epoch:02d}.hdf5'
-    model_file='/home/adhamija/faceness/ImageNet/Ring_Loss_Data/'+args.snapshot_location+'Model_Epoch_number_'+str(args.cross_entropy_loss_weight)+'_'+str(args.ring_loss_weight)+'_{}.hdf5'
-    
+    model_file='/home/adhamija/faceness/ImageNet/Ring_Loss_Data/'+args.snapshot_location+'Model_Epoch_number_'+str(args.cross_entropy_loss_weight)+'_' +str(args.ring_loss_weight)+'_{}.hdf5'
 else:
     model_file='/home/adhamija/faceness/ImageNet/Cross_Entr_Data/'+args.snapshot_location+common_name
-
 
 #model_file='JuNK_Model_Epoch_number_{epoch:02d}.hdf5'
     
     
-class LossHistory(keras.callbacks.Callback):
+class model_saver_cb(keras.callbacks.Callback):
     def __init__(self, model):
         self.model_to_save = model
         
-    def on_train_begin(self, logs={}):
-        self.losses = []
-#        self.soft_loss = []
-
-    def on_batch_end(self, batch, logs={}):
-        pass
-#        self.losses.append(logs.get('loss'))
-#        self.soft_loss.extend([logs.get('softmax_ard_mean_pred').tolist()])
-
     def on_epoch_end(self, epoch, logs={}):
+        print "Saving model at : ",model_file.format(epoch)
         self.model_to_save.save(model_file.format(epoch))
 
-history = LossHistory(model)
-
-checkpointer = ModelCheckpoint(
-                                filepath=model_file, 
-                                verbose=1, 
-                                save_best_only=False , 
-                                period=1, 
-                                save_weights_only=False
-                                )
+model_saver = model_saver_cb(model)
 
 info = parallel_model.fit_generator(
                             generator=training_generator,
                             validation_data=validation_generator,
                             max_queue_size=50,
                             epochs=args.no_of_epochs,
-#                            callbacks=[checkpointer,history],
-                            callbacks=[history],
+                            callbacks=[model_saver],
 #                            verbose=2,
                             **fit_generator_params
                             )
 
-print len(history.losses)
-print (history.losses)
-
-print info
-print info.history
-print info.history.keys()
-
-print history.soft_loss
-print len(history.soft_loss)
-
-
-
-print "global_loss",len(global_loss)
-print global_loss
 #parallel_model.save(model_file)
