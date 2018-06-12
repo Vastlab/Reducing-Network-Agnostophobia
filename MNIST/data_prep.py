64d9b2e45404e8da07386b597f0ef60d02751e60
akshay <adhamija@uccs.edu>
2018-06-12 13:52:12 -0600

    adding Hindi, NotMNIST

--- MNIST/data_prep.py
+++ MNIST/data_prep.py
@@ -4,8 +4,12 @@ import csv
 import gzip
 import struct
 import cv2
+import glob
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
+import multiprocessing
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 
 # https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
 colors = np.array([
@@ -18,7 +22,8 @@ colors = np.array([
     [70, 240, 240],
     [240, 50, 230],
     [210, 245, 60],
-    [250, 190, 190]
+    [250, 190, 190],
+    [0, 0, 0]
 ]).astype(np.float)
 colors=colors/255.
 
@@ -37,8 +42,15 @@ def read_images(fname):
         shape = (n_examples, rows*cols)
         # reads the rest, using an uint8 dataformat (endian-less)
         images = np.fromstring(f.read(), dtype='uint8').reshape(shape)
+        images = 255.-images
         return images
 
+def read_hindi_letters(file_name):
+    img = cv2.imread(file_name,0)
+    img = cv2.resize(img,(28,28),interpolation=cv2.INTER_CUBIC)
+    return img
+
+    
 class mnist_data_prep:
     
     def __init__(self):
@@ -66,13 +78,33 @@ class letters_prep:
         labels=read_labels('/net/kato/datasets/MNIST_Letters/EMNIST_Binary_files/emnist-letters-train-labels-idx1-ubyte.gz')
         images=images*(1./255.)
         images=images.reshape((images.shape[0],28,28,1))
+        images=np.rot90(images, k=3, axes=(1,2))
+        images=np.flip(images,axis=2)
+
         self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(images, labels,train_size=0.8,test_size=0.2)
+        all_classes=list(set(labels.tolist()))
+        known_unknown_classes = all_classes[:int(0.5*len(all_classes))]
+        unknown_classes = all_classes[int(0.5*len(all_classes)):]
+        self.knowns_X_train, self.knowns_X_val, self.knowns_Y_train, self.knowns_Y_val = train_test_split(
+                                                                                            images[np.where(np.isin(labels, known_unknown_classes))], 
+                                                                                            labels[np.where(np.isin(labels, known_unknown_classes))],
+                                                                                            train_size=0.8,
+                                                                                            test_size=0.2
+                                                                                        )
 
         images=read_images('/net/kato/datasets/MNIST_Letters/EMNIST_Binary_files/emnist-letters-test-images-idx3-ubyte.gz')
         labels=read_labels('/net/kato/datasets/MNIST_Letters/EMNIST_Binary_files/emnist-letters-test-labels-idx1-ubyte.gz')
         images=images*(1./255.)
-        self.X_test=images.reshape((images.shape[0],28,28,1))
+        images=images.reshape((images.shape[0],28,28,1))
+        images=np.rot90(images, k=3, axes=(1,2))
+        images=np.flip(l,axis=2)
+
+        self.X_test=images
         self.Y_test=labels
+        
+        self.unknowns_X_test = images[np.where(np.isin(labels, unknown_classes))]
+        self.unknowns_Y_test = labels[np.where(np.isin(labels, unknown_classes))]
+        
         
 class cifar_prep:
     
@@ -85,3 +117,21 @@ class cifar_prep:
             resized_images.append(cv2.resize(gray_img,(28,28),interpolation=cv2.INTER_CUBIC))
         resized_images=np.array(resized_images)[...,np.newaxis]
         self.images=resized_images*(1./255.)
+        
+        
+class hindi_letters:
+    
+    def __init__(self):
+        root_path = '/net/kato/datasets/Hindi_Letters/nhcd/'
+        to_process=['consonants/','vowels/']
+        images=[]
+        p=ThreadPool(multiprocessing.cpu_count())
+        for folder in to_process:
+            files_to_process = glob.glob(root_path+folder+'*/*')
+            raw_data=p.map(read_hindi_letters,files_to_process)
+            images.extend(raw_data)
+        self.images = np.array(images)[...,np.newaxis]
+        self.images = self.images*(1./255.)
+        p.close()
+        p.join()
+        del p
